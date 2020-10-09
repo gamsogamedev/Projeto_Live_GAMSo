6b89b81e066fd7b0febed49602ce4f7bbe3da44c
Yamartim <54243954+Yamartim@users.noreply.github.com>
2020-10-09 17:56:41 -0300

    uploadando projeto

---
+++ Live GAMSo/Assets/inimigo_script.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class inimigo_script : MonoBehaviour
+{
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.tag == "Player")
+        {
+            other.GetComponent<movimento>().HP -= 10;
+            Debug.Log("perdeu vida " + other.GetComponent<movimento>().HP);
+        }
+    }
+
+}
---
+++ Live GAMSo/Assets/movimento.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movimento : MonoBehaviour
+{
+
+    public int HP = 100;
+    public float vel = 1f;
+
+    Rigidbody2D rb2d;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb2d = gameObject.GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetAxis("Horizontal") != 0)
+        {
+            rb2d.velocity = new Vector2(Input.GetAxis("Horizontal")*vel, rb2d.velocity.y);
+        }
+
+        if(Input.GetKey(KeyCode.Space))
+        {
+            rb2d.AddForce(Vector2.up*10f);
+
+        }
+
+        if(HP <= 0)
+        {
+            Destroy(this.gameObject);
+        }
+
+    }
+}
